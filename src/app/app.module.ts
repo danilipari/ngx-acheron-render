650b5fd16724ad4f35650ec08c80c976dfd4a523
Dani Lipari <lipari.assistenza@gmail.com>
2022-09-13 16:41:58 +0200

    update & import lib in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AcheronModule } from 'acheron';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AcheronModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
